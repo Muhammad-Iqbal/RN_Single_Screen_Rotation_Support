bfe3e69dce7f0ea7fe1054d434e4faa9f141306c
Muhammad Iqbal <>
2022-03-25 23:23:24 +0500

    basic signle screen rotation done

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import {View, Text, SafeAreaView, StyleSheet,Pressable} from 'react-native';
 import React from 'react';
+import Orientation from "react-native-orientation";
 
 const App = () => {
+
+  const rotateSingleScreen = () =>{
+    Orientation.unlockAllOrientations();
+    Orientation.lockToPortrait();
+  }
   return (
     <SafeAreaView style={{flex: 1,backgroundColor: '#715ba9'}}>
       <View style={{flex: 1, backgroundColor: '#715ba9'}}>
         <Text style={{fontSize: 50}}>Test Single Screen Rotation</Text>
-        <Pressable style={{alignItems: 'center'}} onPress={()=>alert('rotate')}>
+        <Pressable style={{alignItems: 'center'}} onPress={rotateSingleScreen}>
           <Text>Rotate</Text>
         </Pressable>
       </View>
